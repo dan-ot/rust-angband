d847936531890906d3639a23ac03f994050a2dd5
Dan <dan@overthinker.net>
2021-11-19 12:46:35 -0600

    Moving through object.h

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -123,6 +123,88 @@ pub fn sustain_flag(stat: Stats) -> ObjectFlags {
     }
 }
 
+/// Base: Acid to Cold. Hight: Poison to Disen.
+pub enum Elements {
+    Acid = 0,
+    Elec,
+    Fire,
+    Cold,
+    Pois,
+    Light,
+    Dark,
+    Sound,
+    Shard,
+    Nexus,
+    Nether,
+    Chaos,
+    Disen,
+    Water,
+    Ice,
+    Gravity,
+    Inertia,
+    Force,
+    Time,
+    Plasma,
+    Meteor,
+    Missile,
+    Mana,
+    HolyOrb,
+    Arrow
+}
+
+pub enum Origin {
+    None = 0,
+    Floor,
+    Chest,
+    Special,
+    Pit,
+    Vault,
+    Labyrinth,
+    Cavern,
+    Rubble,
+    Mixed,
+    Drop,
+    DropSpecial,
+    DropPit,
+    DropVault,
+    Stats,
+    Acquire,
+    Store,
+    Stolen,
+    Birth,
+    Cheat,
+    DropBreed,
+    DropSummon,
+    DropUnknown,
+    DropPoly,
+    DropMimic,
+    DropWizard
+}
+
+impl Origin {
+    pub fn describe(&self, subject: &str) -> String {
+        match self {
+            Origin::None | Origin::Mixed | Origin::Stats | Origin::Stolen => String::from(""),
+            Origin::Drop | Origin::DropSpecial | Origin::DropPit | Origin::DropVault
+                | Origin::DropBreed | Origin::DropSummon | Origin::DropPoly
+                | Origin::DropMimic | Origin::DropWizard => "Dropped by ".to_owned() + subject,
+            Origin::DropUnknown => "Dropped by an unknown monster ".to_owned() + subject,
+            Origin::Floor => "Found lying on the floor ".to_owned() + subject,
+            Origin::Chest => "Taken from a chest found ".to_owned() + subject,
+            Origin::Special => "Found lying on the floor of a special room ".to_owned() + subject,
+            Origin::Pit => "Found lying on the floor of a pit ".to_owned() + subject,
+            Origin::Vault => "Found lying on the floor in a vault ".to_owned() + subject,
+            Origin::Labyrinth => "Found lying on the floor of a labyrinth ".to_owned() + subject,
+            Origin::Cavern => "Found lying on the floor of a cavern ".to_owned() + subject,
+            Origin::Rubble => "Found under some rubble ".to_owned() + subject,
+            Origin::Acquire => "Conjured forth by magic ".to_owned() + subject,
+            Origin::Store => "Bought from a store".to_owned(),
+            Origin::Birth => "An inheritance from your family ".to_owned(),
+            Origin::Cheat => "Created by debug option".to_owned(),
+        }
+    }
+}
+
 /// An inventory object. The original serves a lot of purposes - they describe the prototypes
 /// ala Flyweight pattern, and they also store navigation properties to support doubly-linked lists.
 /// It will be difficult to tease all that out properly.
